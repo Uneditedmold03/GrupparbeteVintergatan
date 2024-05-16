1003517e7605d23f2e525eaee3918629fcfb817a
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-05-16 14:50:23 +0200

    added axios post and get request for API

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import axios from "axios"
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.post("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys")
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    axios.get('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies', {
+      headers: { 'x-zocom': "solaris-7BTxHCyHhzIME5TI" }
+    }).then(response1 => {
+      setData(response1.data);
+    })
+    .catch(error => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
-
     <div className="app">
       
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
