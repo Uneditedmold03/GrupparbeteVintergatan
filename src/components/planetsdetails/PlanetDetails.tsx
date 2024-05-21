21c8d74268cb7d9ab2921235e1250a97222589b1
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-05-21 16:46:35 +0200

    added css for planetdetails for description etc etc, added proper structures and classnames for infobox to display different values from planet, changed homescreen color and background img to match planetdetails background

--- src/components/planetsdetails/PlanetDetails.tsx
+++ src/components/planetsdetails/PlanetDetails.tsx
@@ -30,12 +30,40 @@ function Planetdetails({ planetList }: Props) {
       <div className={`planetdetails planetdetails-${planet.name}`}>
 
         <article className="planetinfo">
-        <h1 className="text">{planet.name}</h1>
-        <h2 className="text">{planet.latinName}</h2>
-        <p>{planet.desc}</p>
+          <h1 className="text">{planet.name}</h1>
+          <h2 className="text">{planet.latinName}</h2>
+          <p>{planet.desc}</p>
+          <br />
+          <hr />
+          <br />
+
+          <aside className="infobox">
+            <h3 className="text">OMKRETS</h3>
+            <h3 className="text">KM FRÅN SOLEN</h3>
+            <p>{planet.circumference} km</p>
+            <p>{planet.distance}</p>
+
+            <h3 className="text">MAX TEMPERATUR</h3>
+            <h3 className="text">MIN TEMPERATUR</h3>
+            <p>{planet.temp.day}</p>
+            <p>{planet.temp.night}</p>
+          </aside>
+
+          <br />
+          <hr />
+          <br />
+
+          <aside className="infobox">
+            <h3 className="text">MÅNAR</h3>
+            <h3></h3>
+          </aside>
+
+          <div className="moons">
+            <p>{planet.moons && planet.moons.length > 0 ? planet.moons.join(', ') : 'Har ingen måne'}</p>
+          </div>
 
         </article>
-  
+
       </div>
 
     </section>
