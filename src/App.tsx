6a8f5d53300bdee471c8ff4131763da37785c2e9
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-05-23 15:32:18 +0200

    added planet favorite function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import { useState, useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import axios from 'axios'
-import './App.css'
-import Planets from "./components/Planets/Planets"
-import Planetdetails from './components/planetsdetails/PlanetDetails'
-import Planet from "./models/Planet"
-import Favoriteplanet from './components/Favoriteplanet/Favoriteplanet'
-
+import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import './App.css';
+import Planets from "./components/Planets/Planets";
+import Planetdetails from './components/planetsdetails/PlanetDetails';
+import Planet from "./models/Planet";
+import Favoriteplanet from './components/Favoriteplanet/Favoriteplanet';
 
 function App() {
-  const [key, setKey] = useState<string>("")
-  const [planetList, setPlanetList] = useState<Planet[]>([])
-
+  const [key, setKey] = useState("");
+  const [planetList, setPlanetList] = useState<Planet[]>([]);
+  const [favoritePlanets, setFavoritePlanets] = useState<Planet[]>([]); // Lista över favoritplaneter
 
   useEffect(() => {
     axios.post('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys')
     .then(response => {
-      setKey(response.data.key)
+      setKey(response.data.key);
     })
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (key) {
@@ -32,12 +31,17 @@ function App() {
     }
   }, [key]);
 
+  // Funktion för att spara favoritplanet
+  const saveFavoritePlanet = (planet: Planet) => {
+    setFavoritePlanets(prevFavoritePlanets => [...prevFavoritePlanets, planet]);
+  };
+
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Planets planets={planetList} />} />
-        <Route path="/planet/details/:id" element={<Planetdetails  planetList={planetList} />} />
-        <Route path="/favorite/planets" element={<Favoriteplanet />} />
+        <Route path="/planet/details/:id" element={<Planetdetails planetList={planetList} saveFavoritePlanet={saveFavoritePlanet} />} /> {/* Skicka med funktionen för att spara favoritplanet */}
+        <Route path="/favorite/planets" element={<Favoriteplanet favoritePlanets={favoritePlanets} />} /> {/* Skicka med listan över favoritplaneter */}
       </Routes>
     </div>
   );
