c852881a29ade7e208bc15c9567df98cc5302421
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-05-23 15:38:10 +0200

    m

--- src/components/Planets/PlanetItem.tsx
+++ src/components/Planets/PlanetItem.tsx
@@ -1,60 +1,22 @@
-// import React from "react";
-// import Planet from "../../models/Planet"
-// import './PlanetItem.css'
-// import { Link } from "react-router-dom";
-
-// type Props = {
-//     changeTitle: (newTitle: string) => void,
-//     planet: Planet
-// }
-
-// // function PlanetItem({ changeTitle, planet }: Props) {
-// //     return (
-// //         <Link to={'/planet/details/' + planet.id} className={`planet-${planet.name} planet`}
-// //             onMouseLeave={() => changeTitle('Solaris Space Center')}
-// //             onMouseOver={() => changeTitle(planet.name)}>
-// //             <div ></div></Link>
-// //     )
-// // }
-
-// function PlanetItem({ changeTitle, planet }: Props) {
-//     return (
-//       <div>
-//         <Link to={'/planet/details/' + planet.id} className={`planet-${planet.name} planet`}
-//           onMouseLeave={() => changeTitle('Solaris Space Center')}
-//           onMouseOver={() => changeTitle(planet.name)}>
-//           <div></div>
-//         </Link>
-//       </div>
-//     )
-//   }
-  
-
-
-
-// export default PlanetItem;
-
 import React from "react";
 import Planet from "../../models/Planet"
 import './PlanetItem.css'
 import { Link } from "react-router-dom";
 
 type Props = {
-    changeTitle: (newTitle: string) => void,
-    planet: Planet,
-    addFavorite: (planet: Planet) => void
+  changeTitle: (newTitle: string) => void,
+  planet: Planet
 }
 
-function PlanetItem({ changeTitle, planet, addFavorite }: Props) {
-    return (
-        <div>
-          <Link to={'/planet/details/' + planet.id} className={`planet-${planet.name} planet`}
-            onMouseLeave={() => changeTitle('Solaris Space Center')}
-            onMouseOver={() => changeTitle(planet.name)}>
-            <div></div>
-          </Link>
-          <button onClick={() => addFavorite(planet)}>Add to favorites</button>
-        </div>
+function PlanetItem({ changeTitle, planet }: Props) {
+  return (
+    <div>
+      <Link to={'/planet/details/' + planet.id} className={planet - ${planet.name} planet}
+      onMouseLeave={() => changeTitle('Solaris Space Center')}
+      onMouseOver={() => changeTitle(planet.name)}>
+    </Link >
+    </div >
+
     )
 }
 
