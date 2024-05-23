b409d1317449a01e4effce12a88cc205afada7f2
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-05-23 12:21:40 +0200

    fixed merging issues with main

--- src/components/planetsdetails/PlanetDetails.tsx
+++ src/components/planetsdetails/PlanetDetails.tsx
@@ -5,7 +5,7 @@ import { Link, useParams } from 'react-router-dom'
 
 type Props = {
   planetList: Planet[]
-}                                
+}
 function Planetdetails({ planetList }: Props) {
   const [planet, setPlanet] = useState<Planet | null>(null)
   const { id } = useParams();
@@ -16,7 +16,7 @@ function Planetdetails({ planetList }: Props) {
       if (foundPlanet !== undefined) {
         setPlanet(foundPlanet)
       }
-      
+
     }
   }, [])
 
@@ -27,50 +27,47 @@ function Planetdetails({ planetList }: Props) {
 
     <section className={`planetdetailsbackground`}>
 
-      <div className={`planetdetails planetdetails-${planet.name}`}>
+      <article className="planetinfo">
 
-        <article className="planetinfo">
-          <aside className="info-box-btn">
-            <h1 className="text">{planet.name}</h1>
-            <Link to={'/favorite/planets'}><button className='btn'>Go to favorite planet</button></Link>
-          </aside>
-          <h2 className="text">{planet.latinName}</h2>
-          <p>{planet.desc}</p>
-          <br />
-          <hr />
-          <br />
+        <aside className="info-box-btn">
+          <h1 className="text">{planet.name}</h1>
+          <Link to={'/favorite/planets'}><button className='btn'>Go to favorite planet</button></Link>
+        </aside>
 
-          <aside className="infobox">
-            <h3 className="text">OMKRETS</h3>
-            <h3 className="text">KM FRÅN SOLEN</h3>
-            <p>{planet.circumference} km</p>
-            <p>{planet.distance}</p>
+        <h2 className="text">{planet.latinName}</h2>
+        <p>{planet.desc}</p>
+        <br />
+        <hr />
+        <br />
+        <aside className="infobox">
+          <h3 className="text">OMKRETS</h3>
+          <h3 className="text">KM FRÅN SOLEN</h3>
+          <p>{planet.circumference} km</p>
+          <p>{planet.distance}</p>
 
-            <h3 className="text">MAX TEMPERATUR</h3>
-            <h3 className="text">MIN TEMPERATUR</h3>
-            <p>{planet.temp.day}</p>
-            <p>{planet.temp.night}</p>
-          </aside>
+          <h3 className="text">MAX TEMPERATUR</h3>
+          <h3 className="text">MIN TEMPERATUR</h3>
+          <p>{planet.temp.day}</p>
+          <p>{planet.temp.night}</p>
+        </aside>
 
-          <br />
-          <hr />
-          <br />
+        <br />
+        <hr />
+        <br />
 
-          
-          <aside className="infobox">
-            <h3 className="text">MÅNAR</h3>
-            <h3></h3>
-          </aside>
 
-          <div className="moons">
-            <p>{planet.moons && planet.moons.length > 0 ? planet.moons.join(', ') : 'Har ingen måne'}</p>
-          </div>
+        <aside className="infobox">
+          <h3 className="text">MÅNAR</h3>
+          <h3></h3>
+        </aside>
 
-        </article>
+        <div className="moons">
+          <p>{planet.moons && planet.moons.length > 0 ? planet.moons.join(', ') : 'Har ingen måne'}</p>
+        </div>
 
-      </div>
+      </article>
 
-    </section>
+    </section >
 
 
   )
